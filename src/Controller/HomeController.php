13e6338a1cb92c427f7bb5dcf814c3fdfffd5b73
JoelDeoKL <joeldeocm88@gmail.com>
2023-10-03 10:09:25 +0200

    Mise en place de la structure cote rh

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,9 +8,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/coordonateur', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('admin/index.html.twig');
+    }
+
+    #[Route('/rh', name: 'rh')]
+    public function rh(): Response
+    {
+        return $this->render('rh/index.html.twig');
     }
 }
